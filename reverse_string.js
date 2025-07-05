3bec9ec5f71e7910a85981978fc451cce775a15f
Yograj2024 <175401756+Yograj2024@users.noreply.github.com>
2025-07-05 01:38:00 -0400

    implement reverse string logic

---
+++ reverse_string.js
@@ -0,0 +1,11 @@
+function reverseSting (str){
+    let reversed = '';
+    
+    for(let i=str.length-1; i>=0; i--) 
+        reversed += str[i];
+
+    return reversed;
+} 
+
+const result = reverseSting('hello world');
+// console.log(result);
