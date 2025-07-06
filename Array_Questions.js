e444ff0d332677bfb62fe0d87d57cc8c60b0c8e9
Yograj2024 <175401756+Yograj2024@users.noreply.github.com>
2025-07-06 10:18:00 -0400

    implement js solution for isArray palindrome

--- Array_Questions.js
+++ Array_Questions.js
@@ -30,3 +30,38 @@ console.log(reversedArr); // Output: [6, 5, 4, 3, 2, 1]
 
 
 
+// ============================================
+// FUNCTION 2: Array Palindrome hai ya nahi check karna
+// Palindrome = jo array start se aur end se same dikhta hai
+// Jaise: [1,2,3,2,1] => palindrome hai
+//        [1,2,3,4,5] => palindrome nahi hai
+// ============================================
+
+function isPalindrome(arr) {
+    // Yahan bhi sirf HALF length tak loop chalta hai
+    // Kyunki har pair (start-index aur uske corresponding end-index) ek hi baar compare karna kaafi hai
+    for (let i = 0; i < arr.length / 2; i++) {
+        
+        // START wale element ko uske corresponding END wale element se compare kiya
+        // Agar dono match nahi karte, to array palindrome nahi hai
+        if (arr[i] !== arr[arr.length - 1 - i]) {
+            // Mismatch milte hi turant "No" return kar diya Poore loop ko complete karne ki zarurat nahi 
+            // kyo ki first and last elemtent match nahi karte to palinrome nahi hai  (early exit for efficiency)
+            return ["No", arr];
+        }
+    }
+    
+    // Agar loop poora chal gaya aur koi mismatch nahi mila, to iska matlab array palindrome hai
+    return ["yes", arr];
+}
+
+const palindromeArr = [1, 2, 3, 2, 1];      // Ye palindrome hai
+const nonPalindromeArr = [1, 2, 3, 4, 5];   // Ye palindrome nahi hai
+
+// Function call karke result destructure kiya
+// isPalindrome() ek array return karta hai: [status, originalArray]
+// Destructuring se dono values alag-alag variables me store ho gayi
+const [isPal, passArr] = isPalindrome(nonPalindromeArr);
+console.log(`${isPal} array is palindrome \nand the array is : [${passArr}]`);
+// Output: "No array is palindrome 
+//          and the array is : [1,2,3,4,5]"
